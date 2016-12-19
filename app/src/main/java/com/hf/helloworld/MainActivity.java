bb804325cbee91d2e352b661ae5b12e3b8064692
mirstone <mirstone@126.com>
2016-12-19 14:07:39 +0800

    Initial commit

---
+++ app/src/main/java/com/hf/helloworld/MainActivity.java
@@ -0,0 +1,16 @@
+package com.hf.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView view = (TextView) findViewById(R.id.text);
+    }
+}
